5672a1d26486ac0a09df1b04febd7a95187d48fc
zeturn <62530500+zeturn@users.noreply.github.com>
2023-01-05 14:58:16 +0800

    Create hour.sh

---
+++ hour.sh
@@ -0,0 +1,16 @@
+python3 ./hotsearch/hotsearch.py
+python3 ./hotquestion/hotquestion.py
+
+
+year=`date +%Y `
+month=`date +%m `
+day=`date +%d `
+hour=`date +%H`
+now=$year-$month-$day-$hour
+
+
+git config --global user.email ""
+git config --global user.name "actioner"
+
+git add .
+git commit -m "$now"
